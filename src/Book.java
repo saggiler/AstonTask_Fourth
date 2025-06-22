0eca1ab70f3a325f4fb0edbef1122de6eec04002
Jack Furs <saggiler@gmail.com>
2025-06-23 01:10:21 +0300

    add class book

---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    private String bookTitle;
+    private int bookPrice;
+    private int releaseData;
+
+    public Book(String bookTitle, int bookPrice, int releaseData) {
+        this.bookTitle = bookTitle;
+        this.bookPrice = bookPrice;
+        this.releaseData = releaseData;
+    }
+}
