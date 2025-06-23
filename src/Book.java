17fe80dc537726020382a589ec048ac9f8f0458c
Jack Furs <saggiler@gmail.com>
2025-06-23 22:05:39 +0300

    add methods: equals, hashcode, tostring

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String bookTitle;
     private int bookPrice;
@@ -7,5 +9,27 @@ public class Book {
         this.bookTitle = bookTitle;
         this.bookPrice = bookPrice;
         this.releaseData = releaseData;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return bookPrice == book.bookPrice && releaseData == book.releaseData && Objects.equals(bookTitle, book.bookTitle);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookTitle, bookPrice, releaseData);
+    }
+
+    @Override
+    public String toString() {
+        return "Книга: " +
+                "Название книги " + bookTitle + '\'' +
+                " цена: " + bookPrice +
+                " год издания: " + releaseData +
+                '}';
     }
 }
