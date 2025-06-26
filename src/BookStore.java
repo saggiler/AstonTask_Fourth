fb2cdebb113e98f11318a9a4d7f7bfad1926ce36
Jack Furs <saggiler@gmail.com>
2025-06-26 22:06:26 +0300

    add method sellBook

--- src/BookStore.java
+++ src/BookStore.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class BookStore {
     HashSet<Book> listOfBooks = new HashSet<>();
@@ -7,5 +8,19 @@ public class BookStore {
         listOfBooks.add(book);
     }
 
-
+    public void sellBook(String title) {
+        boolean found = false;
+        Iterator<Book> iterator = listOfBooks.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getBookTitle().equalsIgnoreCase(title)) {
+                iterator.remove();
+                found = true;
+            }
+        }
+        if (found) {
+            System.out.println("Книга: " + title + " продана.");
+        } else {
+            System.out.println("Книги: " + title + " отсутствует.");
+        }
+    }
 }
